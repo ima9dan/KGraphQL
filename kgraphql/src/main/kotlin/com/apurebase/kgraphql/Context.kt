b86290e91f4a230b0d6b2aaa0b631f1a7cb2812a
FFN <>
2022-04-15 20:45:37 +0900

    Add Context to setValue Method and getValueMethod. it is for pass data Query to DataLoader.

--- kgraphql/src/main/kotlin/com/apurebase/kgraphql/Context.kt
+++ kgraphql/src/main/kotlin/com/apurebase/kgraphql/Context.kt
@@ -6,6 +6,11 @@ import kotlin.reflect.KClass
 @NotIntrospected
 class Context(private val map: Map<Any, Any>) {
 
+    private var flexibleMap:MutableMap<Any, Any>
+    init {
+        flexibleMap = mutableMapOf()
+    }
+
     operator fun <T : Any> get(kClass: KClass<T>): T? {
         val value = map[kClass]
         @Suppress("UNCHECKED_CAST")
@@ -14,4 +19,10 @@ class Context(private val map: Map<Any, Any>) {
 
     inline fun <reified T : Any> get() : T? = get(T::class)
 
+    fun <T : Any>getValue(key: String): T? {
+        return if (flexibleMap.containsKey(key)) flexibleMap[key] as T else null
+    }
+    fun setValue(key: String, value:Any) {
+        flexibleMap[key] = value
+    }
 }
